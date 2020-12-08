2b0bf5a314ffb78eb342ab764e74fd6c03df474c
Sean Oyler <sean.oyler@gmail.com>
2020-12-08 15:11:01 -0800

    Adds Show component of Appointmnet

--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,7 @@ import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Application";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
 
 storiesOf("Button", module)
   .addParameters({
@@ -138,4 +139,12 @@ storiesOf("Button", module)
           .add("Appointment", () => <Appointment />)
           .add("Appointment with Time...", () => <Appointment time = "12pm" />)
           .add("Header", () => <Header time = "12pm" />)
-          .add("Empty", () => <Empty onAdd = {action("onAdd")}/>);
+          .add("Empty", () => <Empty onAdd = {action("onAdd")}/>)
+          .add("Show", () => (
+          <Show 
+            student = "Lydia Miller-Jones" 
+            interviewer = {interviewers[1].name} 
+            onEdit = {action("onEdit")}
+            onDelete = {action("onDelete")}
+            />
+          ));
