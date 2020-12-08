8e95843b8361df5a214d16199458cb97bb3b0746
Sean Oyler <sean.oyler@gmail.com>
2020-12-08 11:56:13 -0800

    Adds keys to component DayList

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -4,9 +4,8 @@ import DayListItem from "components/DayListItem";
 export default function DayList(props) {
   const day = props.days.map((day) => {
     return (
-      <ul>
+      <ul key = {day.id}>
         <DayListItem 
-        key = {day.id}
         name = {day.name}
         spots = {day.spots}
         selected = {day.name === props.day}
