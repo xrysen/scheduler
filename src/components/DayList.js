2a22f45f545467aab0c94276c3bc892695a3c7de
Sean Oyler <sean.oyler@gmail.com>
2020-12-07 16:42:56 -0800

    Updates application component to use DayList

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,10 +2,11 @@ import React from "react";
 import DayListItem from "components/DayListItem";
 
 export default function DayList(props) {
-  const day = props.days.map(day => {
+  const day = props.days.map((day) => {
     return (
       <ul>
         <DayListItem 
+        key = {day.id}
         name = {day.name}
         spots = {day.spots}
         selected = {day.name === props.day}
