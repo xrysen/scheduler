2e6e038336cab80f25683e37cc0de7ac6502cc19
Sean Oyler <sean.oyler@gmail.com>
2020-12-08 14:45:39 -0800

    Adds Appointment Header component

--- stories/index.js
+++ stories/index.js
@@ -11,6 +11,8 @@ import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
 
+import Appointment from "components/Application"
+
 storiesOf("Button", module)
   .addParameters({
     backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
@@ -126,3 +128,10 @@ storiesOf("Button", module)
             setInterviewer={action("setInterviewer")}
           />
         ));
+
+        storiesOf("Appointment", module)
+          .addParameters({
+            backgrounds: [{name: "white", value: "#fff", default: true}]
+          })
+          .add("Appointment", () => <Appointment />)
+          .add("Time", () => <Appointment time = "12pm" />);
