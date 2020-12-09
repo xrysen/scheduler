2915128c2c1e9d9816d2f5619a1db78330234316
Sean Oyler <sean.oyler@gmail.com>
2020-12-08 21:50:39 -0800

    Adds functionality to Forms component

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,33 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import InterviewerList from "components/InterviewerList";
 import Button from "components/Button";
 import "./styles.scss";
 
 export default function Form(props) {
+  const [name, setName] = useState(props.name || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  const reset = () => {
+    setName("");
+    setInterviewer(null);
+  };
+
+  const cancel = () => {
+    reset();
+    props.onCancel();
+  };
+
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form autoComplete="off" onSubmit={(event) => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
             type="text"
             placeholder="Enter Student Name"
-            value = {props.name}
+            onChange={(event) => setName(event.target.value)}
+            value={name}
           />
         </form>
         <InterviewerList
           interviewers={props.interviewers}
-          name = {props.name}
-          id = {props.id}
-          interviewer = {props.interviewer}
-          setInterviewer = {props.setInterviewer}
+          name={props.name}
+          id={props.id}
+          interviewer={interviewer}
+          setInterviewer={setInterviewer}
         />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick = {props.onCancel}>Cancel</Button>
-          <Button confirm onClick = {props.onSave}>Save</Button>
+          <Button danger onClick={cancel}>
+            Cancel
+          </Button>
+          <Button confirm onClick={() => props.onSave(name, interviewer)}>
+            Save
+          </Button>
         </section>
       </section>
     </main>
