02ee620488e1687737b8805aee1e4e1867684071
Sean Oyler <sean.oyler@gmail.com>
2020-12-16 18:04:00 -0800

    Removes act warning from jest tests

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -5,7 +5,6 @@ import {
   cleanup,
   waitForElement,
   getByText,
-  prettyDOM,
   getAllByTestId,
   getByPlaceholderText,
   getByAltText,
--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -1,10 +1,11 @@
 import React from "react";
 
-import { render } from "@testing-library/react";
+import { render, waitForElement, getByText } from "@testing-library/react";
 import Application from "../Application";
 
 describe("Appointment", () => {
-  it("renders without crashing", () => {
-    render(<Application />);
+  it("renders without crashing", async () => {
+    const { container } = render(<Application />);
+    await waitForElement(() => getByText(container, "Archie Cohen")); 
   });
 });
